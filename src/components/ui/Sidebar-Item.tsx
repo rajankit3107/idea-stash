b3665421a631245904dcf9b6d3954baf05894cf3
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-01 14:39:54 +0530

    feat : updated the sidebar

--- src/components/ui/Sidebar-Item.tsx
+++ src/components/ui/Sidebar-Item.tsx
@@ -7,7 +7,7 @@ interface SideBarItemProps {
 
 export function SideBarItem({ text, icon }: SideBarItemProps) {
   return (
-    <div className="flex">
+    <div className="flex cursor-pointer hover:bg-gray-100 rounded max-w-48 pl-4 transition-all duration-200">
       <div className="p-2">{icon}</div>
       <div className="p-2">{text}</div>
     </div>
--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Logo } from "../../icons/Logo";
 import { TwitterIcon } from "../../icons/TwitterIcon";
 import { YoutubeIcon } from "../../icons/YoutubeIcon";
 import { SideBarItem } from "./Sidebar-Item";
@@ -5,8 +6,13 @@ import { SideBarItem } from "./Sidebar-Item";
 export function SideBar() {
   return (
     <div className="h-screen bg-white border-r w-60 fixed left-0 top-0 pl-6">
-      <div className="flex text-2xl pt-4">Idea-Stash</div>
-      <div className="pt-4">
+      <div className="flex text-2xl pt-6 items-center">
+        <div className="pr-2">
+          <Logo />
+        </div>
+        Idea-Stash
+      </div>
+      <div className="pt-6">
         <SideBarItem text="Twitter" icon={<TwitterIcon />} />
         <SideBarItem text="Youtube" icon={<YoutubeIcon />} />
       </div>
